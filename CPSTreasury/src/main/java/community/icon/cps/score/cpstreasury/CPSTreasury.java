d731c2415edba30bd8c77f5be99381280e838cba
parajuliswopnil <parajuliswopnil717@gmail.com>
2022-05-02 09:56:33 +0545

    changed the variable type

--- CPSTreasury/src/main/java/community/icon/cps/score/cpstreasury/CPSTreasury.java
+++ CPSTreasury/src/main/java/community/icon/cps/score/cpstreasury/CPSTreasury.java
@@ -160,7 +160,7 @@ public class CPSTreasury extends ProposalData {
     public Map<String, ?> get_contributor_projected_fund(Address _wallet_address) {
         BigInteger totalAmountToBePaidICX = BigInteger.ZERO;
         BigInteger totalAmountToBePaidbnUSD = BigInteger.ZERO;
-        List<Map<String, String>> projectDetails = new ArrayList<>();
+        List<Map<String, ?>> projectDetails = new ArrayList<>();
         for (int i = 0; i < proposalsKeys.size(); i++) {
             String _ipfs_key = proposalsKeys.get(i);
             String proposalPrefix = proposalPrefix(_ipfs_key);
@@ -174,7 +174,7 @@ public class CPSTreasury extends ProposalData {
                         BigInteger totalBudget = (BigInteger) proposal_details.get(consts.TOTAL_BUDGET);
                         BigInteger totalPaidAmount = (BigInteger) proposal_details.get(consts.WITHDRAW_AMOUNT);
 
-                        Map<String, String> project_details = Map.of(
+                        Map<String, ?> project_details = Map.of(
                                 consts.IPFS_HASH, _ipfs_key,
                                 consts.TOKEN, flag,
                                 consts.TOTAL_BUDGET, totalBudget.toString(),
