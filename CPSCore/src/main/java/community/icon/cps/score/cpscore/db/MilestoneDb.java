3b91cbd8c2e45f174509a96e56f89b76ca84b00a
naneey <nanneychan03@gmail.com>
2023-10-04 16:13:44 +0545

    chore: total votes stored in progress report db

--- CPSCore/src/main/java/community/icon/cps/score/cpscore/db/MilestoneDb.java
+++ CPSCore/src/main/java/community/icon/cps/score/cpscore/db/MilestoneDb.java
@@ -12,13 +12,11 @@ public class MilestoneDb {
     public static final BranchDB<String, VarDB<Integer>> id = Context.newBranchDB(MILESTONE_ID, Integer.class);
     public static final BranchDB<String, VarDB<String>> progressReportHash = Context.newBranchDB(REPORT_HASH, String.class);
     public static final BranchDB<String, VarDB<Integer>> status = Context.newBranchDB(STATUS, Integer.class);
-    public static final BranchDB<String, VarDB<BigInteger>> totalVotes = Context.newBranchDB(TOTAL_VOTES, BigInteger.class);
     public static final BranchDB<String, VarDB<BigInteger>> approvedVotes = Context.newBranchDB(APPROVED_VOTES, BigInteger.class);
     public static final BranchDB<String, VarDB<BigInteger>> rejectedVotes = Context.newBranchDB(REJECTED_VOTES, BigInteger.class);
     public static final BranchDB<String, ArrayDB<Address>> votersList = Context.newBranchDB(VOTERS_LIST, Address.class);
     public static final BranchDB<String, ArrayDB<Address>> approveVoters = Context.newBranchDB(APPROVE_VOTERS, Address.class);
     public static final BranchDB<String, ArrayDB<Address>> rejectVoters = Context.newBranchDB(REJECT_VOTERS, Address.class);
-    public static final BranchDB<String, VarDB<Integer>> totalVoters = Context.newBranchDB(TOTAL_VOTERS, Integer.class);
 
     public static final BranchDB<String, BranchDB<Address, DictDB<String, Integer>>> votersListIndices = Context.newBranchDB(VOTERS_LIST_INDEXES, Integer.class);
 
@@ -26,22 +24,21 @@ public class MilestoneDb {
         id.at(prefix).set(milestoneData.id);
         status.at(prefix).set(MILESTONE_REPORT_SUBMITTED);
         progressReportHash.at(prefix).set(milestoneData.reportHash);
-        totalVotes.at(prefix).set(BigInteger.ZERO);
-        totalVoters.at(prefix).set(0);
         approvedVotes.at(prefix).set(BigInteger.ZERO);
         rejectedVotes.at(prefix).set(BigInteger.ZERO);
 
     }
 
     public static Map<String, Object> getDataFromMilestoneDB(String prefix) {
+        String reportHash = progressReportHash.at(prefix).getOrDefault("");
         return Map.ofEntries(
                 Map.entry(MILESTONE_ID, id.at(prefix).get()),
                 Map.entry(STATUS, status.at(prefix).getOrDefault(0)),
                 Map.entry(REPORT_HASH, progressReportHash.at(prefix).getOrDefault("")),
-                Map.entry(TOTAL_VOTES, totalVotes.at(prefix).getOrDefault(BigInteger.ZERO)),
+                Map.entry(TOTAL_VOTES, ProgressReportDataDb.totalVotes.at(progressReportPrefix(reportHash)).getOrDefault(BigInteger.ZERO)),
                 Map.entry(APPROVED_VOTES, approvedVotes.at(prefix).getOrDefault(BigInteger.ZERO)),
                 Map.entry(REJECTED_VOTES, rejectedVotes.at(prefix).getOrDefault(BigInteger.ZERO)),
-                Map.entry(TOTAL_VOTERS, totalVoters.at(prefix).getOrDefault(0)),
+                Map.entry(TOTAL_VOTERS, ProgressReportDataDb.totalVoters.at(progressReportPrefix(reportHash)).getOrDefault(0)),
                 Map.entry(APPROVE_VOTERS, approveVoters.at(prefix).size()),
                 Map.entry(REJECT_VOTERS, rejectVoters.at(prefix).size()));
     }
