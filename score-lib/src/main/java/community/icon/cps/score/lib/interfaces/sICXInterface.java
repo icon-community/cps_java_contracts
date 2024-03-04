b49f4115da040c36666ccfd6b33f7ec1aaddb5a6
naneey <nanneychan03@gmail.com>
2024-03-04 13:13:28 +0545

    chore: dummy files ignored for now

--- score-lib/src/main/java/community/icon/cps/score/lib/interfaces/sICXInterface.java
+++ score-lib/src/main/java/community/icon/cps/score/lib/interfaces/sICXInterface.java
@@ -1,10 +1,10 @@
-package community.icon.cps.score.lib.interfaces;
-
-import community.icon.cps.score.lib.interfaces.tokens.IRC2;
-import foundation.icon.score.client.ScoreClient;
-import foundation.icon.score.client.ScoreInterface;
-
-@ScoreInterface
-@ScoreClient
-public interface sICXInterface extends IRC2 {
-}
+//package community.icon.cps.score.lib.interfaces;
+//
+//import community.icon.cps.score.lib.interfaces.tokens.IRC2;
+//import foundation.icon.score.client.ScoreClient;
+//import foundation.icon.score.client.ScoreInterface;
+//
+////@ScoreInterface(suffix = "Client")
+//@ScoreClient
+//public interface sICXInterface extends IRC2 {
+//}
