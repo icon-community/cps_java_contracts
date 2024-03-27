7e922d87457be8b20b8bbbe37647fb099f8dc5b6
naneey <nanneychan03@gmail.com>
2024-03-27 15:15:03 +0545

    chore:interface added

--- test-lib/src/main/java/community/icon/cps/score/test/integration/scores/sICX.java
+++ test-lib/src/main/java/community/icon/cps/score/test/integration/scores/sICX.java
@@ -1,4 +1,16 @@
 package community.icon.cps.score.test.integration.scores;
 
+import score.Address;
+import score.annotation.External;
+import score.annotation.Optional;
+
+import java.math.BigInteger;
+
 public interface sICX {
+
+    @External
+    void mintTo(Address _account, BigInteger _amount);
+
+    @External
+    void transfer(Address _to, BigInteger _value, @Optional byte[] _data);
 }
