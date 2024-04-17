aed121da9053d1058c017907226026e87e6be475
naneey <nanneychan03@gmail.com>
2024-04-17 14:54:07 +0545

    chore: oracle method name fixed

--- dummy/oracle/src/main/java/dummy/contract/oracle/Oracle.java
+++ dummy/oracle/src/main/java/dummy/contract/oracle/Oracle.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class Oracle {
 
     @External(readonly = true)
-    public Map<String,BigInteger> getReferenceData(String _base, String _quote) {
+    public Map<String,BigInteger> get_reference_data(String _base, String _quote) {
         return Map.of("rate",BigInteger.ONE);
     }
 
